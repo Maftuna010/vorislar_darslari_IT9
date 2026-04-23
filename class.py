58b1cc7ca91dfedb9a5bd1a5e067e6c44a2d7c0e
Maftuna010 <niyozmetovmuzaffar11@gmail.com>
2026-04-23 16:48:30 +0500

    mmmm
    bbbbbb

---
+++ class.py
@@ -0,0 +1,31 @@
+# class Talaba: 
+#     def __int__(self,ism,familiyasi,tyil,otasining_ismi):
+#         self.name = ism 
+#         self.surname = familiya
+#         self.brthday = tyil 
+#         self.ochestva = otasining_ismi
+# def FIO(self):
+#     return f"{self.surname.title()} {self.name}  {self.ochestva}"
+# def yoshi(self):
+#     return f"{2026-self.tyil} - yoshda"
+
+
+# t1 = Talaba('maftuna','madaminova','muzaffarovna',2010)
+# t2 = Talaba ('matluba','ollayorova','bektemirovna',2010)
+# t3 = Talaba ('mashxura','shomurotova','narimonovna',2010)
+# print(t1.FIO(),t1.yosh(), t2.FIO(),t2.yosh(), t3.FIO(),t3.yosh())
+
+
+
+class Kitob:
+   def__int__(self,nomi,muallifi,muqovasasi,narx,yili):
+        self.name = nomi
+        self.muallifi = muallifi
+        self.muqovasi = beti
+        self.narx = narx
+        self.yili = yili
+def malumot(self):
+    return f"{self.nomi} {self.muallifi.title()} {self.muqovasi}  {self.narx} {self.yili}"
+k1 = Kitob('Ikki eshik orasi','otkir hoshimov',700, 85, 1995)
+k2 = Kitob('Choliqushi', 'rashod nuri gultekin', 500, 70, 2000)
+k3 = Kitob('Ufq','said ahmad',750, 85, 1995)
